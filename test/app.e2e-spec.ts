48eb336ce5a9885e8985756c56579953c8a667db
vishnucprasad <mail@vishnucprasad.in>
2023-07-28 11:16:49 +0530

    Setup e2e test for user registration API

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,114 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { request, spec } from 'pactum';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { AppModule } from '../src/app.module';
+import { User } from '../src/user/entity';
+import { Repository } from 'typeorm';
+import { RegisterDto } from 'src/user/dto';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+beforeAll(async () => {
+  const moduleRef = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+  app = moduleRef.createNestApplication();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.init();
+  app.listen(3001);
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  request.setBaseUrl('http://localhost:3001');
+
+  const userRepo: Repository<User> = moduleRef.get(getRepositoryToken(User));
+  userRepo.clear();
+});
+
+afterAll(() => {
+  app.close();
+});
+
+describe('User /user', () => {
+  describe('POST /user/register', () => {
+    it('should throw an error if no body is provided', () => {
+      return spec().post('/user/register').expectStatus(400);
+    });
+
+    it('should throw an error if firstName is empty', () => {
+      const dto: Omit<RegisterDto, 'firstName'> = {
+        lastName: 'Doe',
+        email: 'johndoe@example.com',
+        password: 'JohnDoe@123',
+      };
+
+      return spec().post('/user/register').withBody(dto).expectStatus(400);
+    });
+
+    it('should throw an error if lastName is empty', () => {
+      const dto: Omit<RegisterDto, 'lastName'> = {
+        firstName: 'John',
+        email: 'johndoe@example.com',
+        password: 'JohnDoe@123',
+      };
+
+      return spec().post('/user/register').withBody(dto).expectStatus(400);
+    });
+
+    it('should throw an error if email is empty', () => {
+      const dto: Omit<RegisterDto, 'email'> = {
+        firstName: 'John',
+        lastName: 'Doe',
+        password: 'JohnDoe@123',
+      };
+
+      return spec().post('/user/register').withBody(dto).expectStatus(400);
+    });
+
+    it('should throw an error if password is empty', () => {
+      const dto: Omit<RegisterDto, 'password'> = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@example.com',
+      };
+
+      return spec().post('/user/register').withBody(dto).expectStatus(400);
+    });
+
+    it('should register new user', () => {
+      const dto: RegisterDto = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@example.com',
+        password: 'JohnDoe@123',
+      };
+
+      return spec()
+        .post('/user/register')
+        .withBody(dto)
+        .expectStatus(201)
+        .expectBodyContains(dto.firstName)
+        .expectBodyContains(dto.lastName)
+        .expectBodyContains(dto.email);
+    });
+
+    it('should throw an error if email is already in use', () => {
+      const dto: RegisterDto = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@example.com',
+        password: 'JohnDoe@123',
+      };
+
+      return spec()
+        .post('/user/register')
+        .withBody(dto)
+        .expectStatus(403)
+        .expectBodyContains(dto.email);
+    });
   });
 });
